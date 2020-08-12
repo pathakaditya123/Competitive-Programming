51cb8173ee95417731424971204faeaf2a2983b1
ayush-kumar774 <ayushkumar1234@protonmail.com>
2020-08-12 09:14:34 +0530

    usual commit

--- Codeforces/A. Even Subset Sum Problem.cpp
+++ Codeforces/A. Even Subset Sum Problem.cpp
@@ -1,44 +1,42 @@
 // https://codeforces.com/problemset/problem/1323/A
-
+    
 #include <bits/stdc++.h>
 using namespace std;
 #define int long long
 #define fast ios_base::sync_with_stdio(false) ; cin.tie(0) ; cout.tie(0) ;
 #define endl '\n'
-
+    
 void solve()
 {
-    int n ;
+    int n , index = 0 ;
     cin >> n ;
     vector<int> v (n) ;
-    for(auto &x : v) cin >> x ;
-    int sum = 0 , flag = 0;
-    vector<int> index ;
-    for(int i = 0 ; i < n ; i++)
-    {
-        sum += v[i] ;
-        if(v[i] % 2 == 0) {
-            flag = 1 ;
-            cout << i + 1 << endl ;
-        }        
-        else
-        {
-            if (v [i + 1 ] % 2 == 0) {
-                sum = 0 ;
-                index.clear() ;
-            }
-            else
-            {
-                index.push_back(i + 1) ;
-            }
-            
+    for(int i = 0 ; i < n ; i++) {
+        cin >> v[i] ;
+        if (v[i] % 2 == 0) {
+            index = i + 1 ; 
         }
     }
-    if (flag == 0 ) cout << -1 << endl ;
+    if (index) cout << 1 << endl << index << endl ;
+    else if (n == 1 and index == 0 ) cout << -1 << endl ; 
+    else
+    {
+        cout << 2 << endl ;
+        int pos1 = 0 ;
+        for(int i = 0 ; i < n ; i++) {
+            if (v[i] % 2 != 0 and v[i+1] % 2 != 0) {
+                pos1 = i ;
+                break ;
+            }
+        }
+        cout << pos1 + 1 << " " << pos1 + 2 << endl ;
+    }
+    
 }
-
+    
 int32_t main()
 {
+    fast;
     int testcases ;
     cin >> testcases ;
     while(testcases--)
@@ -48,10 +46,10 @@ int32_t main()
     return 0 ;
 }
 /*  CODED BY:-
- ___________________________________
+    ___________________________________
 |                       ___         |
 |  /\   /\  \ /  |  |  |___   |__|  |        
 | /~~\ /~~\  |   |__|   ___|  |  |  |
 |___________________________________|
- 
+    
 */
