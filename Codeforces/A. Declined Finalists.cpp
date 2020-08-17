345712991e13c8d85579e706118fe5dc32d94b50
ayush-kumar774 <ayushkumar1234@protonmail.com>
2020-08-17 21:03:42 +0530

    usual commit

---
+++ Codeforces/A. Declined Finalists.cpp
@@ -0,0 +1,36 @@
+// https://codeforces.com/problemset/problem/859/A
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define fast ios_base::sync_with_stdio(false) ; cin.tie(0) ; cout.tie(0) ;
+#define endl '\n'
+
+void solve()
+{
+    int n ;
+    cin >> n ;
+    vector<int> v(n) ;
+    for(auto &x : v) cin >> x ;
+    int m = *max_element(v.begin(), v.end()) ;
+    if (m > 25) cout << m - 25 << endl ;
+    else cout << 0 << endl ;
+}
+int32_t main()
+{
+    fast ;
+    int testcases = 1 ;
+    //cin >> testcases ;
+    while(testcases--)
+    {
+        solve() ;
+    }
+    return 0 ;
+}
+/*  CODED BY:-
+ ___________________________________
+|                       ___         |
+|  /\   /\  \ /  |  |  |___   |__|  |        
+| /~~\ /~~\  |   |__|   ___|  |  |  |
+|___________________________________|
+ 
+*/
