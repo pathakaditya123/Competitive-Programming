f823c965ad19a4639f30643fd7f6cdb5f920a1c3
ayush-kumar774 <ayushkumar1234@protonmail.com>
2020-09-05 15:12:53 +0530

    usual commit

--- Codeforces/A. Left-handers, Right-handers and Ambidexters.java
+++ Codeforces/A. Left-handers, Right-handers and Ambidexters.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class LRA {
+class LRA {
         public static void main(String args[]) {
                 Scanner sc = new Scanner(System.in);
                 int l , r , a ;
