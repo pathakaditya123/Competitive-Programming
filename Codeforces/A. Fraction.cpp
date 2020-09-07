58421472ba0a81c5cbfdbba4db2234fb8acb7910
ayush-kumar774 <ayushkumar1234@protonmail.com>
2020-09-07 06:18:28 +0530

    usual commit

--- Codeforces/A. Fraction.cpp
+++ Codeforces/A. Fraction.cpp
@@ -6,7 +6,17 @@ using namespace std;
 
 void solve()
 {
-    
+    int n ;
+    cin >> n ;
+    if (n % 2 == 0) {
+        if ( (n / 2 ) & 1 ) {
+            cout << (n / 2) - 2 << " " << n - ((n / 2) - 2) << endl ;
+        }
+        else cout << (n / 2 ) - 1 << " " << (n / 2 ) + 1 << endl ;
+    }
+    else {
+        cout << (n / 2) << " " << n - (n / 2) << endl ;
+    }
 }
 int32_t main()
 {
