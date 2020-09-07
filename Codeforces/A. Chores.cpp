a0f717409f7aad1f7e9babd1305bfafdada088e5
ayush-kumar774 <ayushkumar1234@protonmail.com>
2020-09-07 11:43:29 +0530

    usual commit

--- Codeforces/A. Chores.cpp
+++ Codeforces/A. Chores.cpp
@@ -3,7 +3,7 @@
 #include <bits/stdc++.h>
 using namespace std ;
 #define int long long
-#define fast ios_base::sync_with_stdio(false) ; cin.tie(0); cout.tie(0) ;
+#define fast ios_base::sync_with_stdio(false) ; cain.tie(0); cout.tie(0) ;
 #define endl '\n'
  
 void solve()
